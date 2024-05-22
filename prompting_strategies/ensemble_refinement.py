c7c891d2481606bd92196ccc75ded0f0335c4b1c
jayson1200 <jaysonmeribe2004@gmail.com>
2024-05-22 15:28:39 -0700

    debugging

--- prompting_strategies/ensemble_refinement.py
+++ prompting_strategies/ensemble_refinement.py
@@ -1,8 +1,13 @@
 PROJ_PATH = "/home/meribejayson/Desktop/Projects/UltimateMedLLM-Llama3-8B/"
-SYS_PROMPT = """The following are multiple choice questions about medical knowledge. Solve them in a step-by-step fashion,
-starting by summarizing the available information. Output a single option from the four options as the final answer. We
-provide several student reasonings for the last question. Some of them may be correct and some incorrect. You can use the
-best correct arguments from these reasonings. Beware of wrong reasoning and do not repeat wrong reasoning."""
+STEP_ONE_SYS_PROMPT = """The following are multiple choice questions about medical knowledge. Solve them in a step-by-step fashion,
+starting by summarizing the available information. Then provide an explanation of your reasoning. Then provide a single option from the four options as the final answer. 
+We provide two example prompts, summaries, explanations, and answers, fill in the last example continuing from what was provided."""
+
+STEP_TWO_SYS_PROMPT = """The following are multiple choice questions about medical knowledge. Solve them in a step-by-step fashion,
+starting by summarizing the available information. Then provide an explanation of your reasoning. Then provide the answer. 
+Output a single option from the four options as the final answer. We provide several student reasonings for the last question as well as two example prompts, summaries, explanations, and answers. 
+Some of them may be correct and some incorrect. You can use the best correct arguments from these reasonings. 
+Beware of wrong reasoning and do not repeat wrong reasoning. We provide two example prompts, summaries, explanations, and answers, fill in the last example continuing from what was provided."""
 
 import sys
 import numpy as np
@@ -17,7 +22,8 @@ class EnsembleRefinement:
                  num_resp_first_stage=11, 
                  num_resp_second_stage=33) -> None:
         
-        self.model = model(SYS_PROMPT)
+        self.step_one_model = model(STEP_ONE_SYS_PROMPT)
+        self.step_two_model = model(STEP_TWO_SYS_PROMPT)
         self.num_resp_first_stage = num_resp_first_stage
         self.num_resp_second_stage = num_resp_second_stage
         self.prompt = prompt
@@ -27,16 +33,16 @@ class EnsembleRefinement:
         
     def run_first_stage(self): 
         for i in range(self.num_resp_first_stage):
-            self.model.set_curr_temperature(np.random.uniform(0.0, 2.0))
-            self.first_stage_concat_out += ("\n" + self.model.send_chat_message(self.prompt))
-            self.model.reset_curr_gen_config()
+            self.step_one_model.set_curr_temperature(np.random.uniform(0.0, 2.0))
+            self.first_stage_concat_out += ("\n" + self.step_one_model.send_chat_message(self.prompt))
+            self.step_one_model.reset_curr_gen_config()
 
     def run_second_stage(self):
-        model_input = self.first_stage_concat_out + "\n" + self.prompt
+        model_input = "Student Reasonings:\n" + self.first_stage_concat_out + "\n" + self.prompt
 
         for i in range(self.num_resp_second_stage):
-            self.model.clear_chat()
-            self.second_stage_resp.append(self.model.send_chat_message(model_input))
+            self.step_two_model.clear_chat()
+            self.second_stage_resp.append(self.step_two_model.send_chat_message(model_input))
 
         return self.second_stage_resp
     
--- src/test.py
+++ src/test.py
@@ -7,23 +7,60 @@ from evaluators.mcq_evaluator import MCQEvaluator
 from prompting_strategies.ensemble_refinement import EnsembleRefinement
 from simplified_llm_apis.gemini import Gemini
 
-exp_q = """Question: A 21-year-old man presents to the physician with complaint of fever and non-bloody diarrhea for the past 3 days, after a week of constipation. He and his family recently returned from a summer spent in New Delhi, India visiting relatives. Physical examination reveals abdominal tenderness and a pink macular rash extending from his trunk to his upper arms. His vital signs are as follows: temperature is 99.7°F (37.6°C), blood pressure is 120/72 mmHg, pulse is 85/min, and respirations are 16/min. Which of the following drugs would be most effective in treating this patient’s condition?
+exp_q = """[Question] 
+A 21-year-old man presents to the physician with complaint of fever and non-bloody diarrhea for the past 3 days, after a week of constipation. He and his family recently returned from a summer spent in New Delhi, India visiting relatives. Physical examination reveals abdominal tenderness and a pink macular rash extending from his trunk to his upper arms. His vital signs are as follows: temperature is 99.7°F (37.6°C), blood pressure is 120/72 mmHg, pulse is 85/min, and respirations are 16/min. Which of the following drugs would be most effective in treating this patient’s condition?
 
-Choices: A: Ciprofloxacin, B: Metronidazole, C: Oral rehydration solution, D: Oral vancomycin, E: Penicillin
+[Choices] 
+A: Ciprofloxacin, B: Metronidazole, C: Oral rehydration solution, D: Oral vancomycin, E: Penicillin
+
+[Summary] 
+A 21-year-old man presents with fever and diarrhea after constipation. After returning from a summer trip, he shows abdominal tenderness and a pink macular rash. Vital signs include temperature, blood pressure, pulse, and respirations.
+
+[Explanation]
+In the context of recent travel to India and the described symptoms, this presentation is highly suggestive of typhoid fever, which is caused by Salmonella typhi. Typhoid fever commonly presents with a stepwise fever, abdominal pain, and a characteristic "rose spots" rash, which are pink macules that typically appear on the trunk and spread to the extremities.
+The most effective treatment for typhoid fever in this scenario is Ciprofloxacin (Choice A), a fluoroquinolone antibiotic. Ciprofloxacin is commonly used to treat enteric fever, including typhoid fever, due to its efficacy against Salmonella typhi and its ability to reach high intracellular concentrations.
+Other Choices:
+Metronidazole (Choice B) is primarily used to treat infections caused by anaerobic bacteria and certain protozoa, such as Giardia and Entamoeba histolytica, but is not effective against Salmonella typhi.
+Oral rehydration solution (Choice C) is important for managing dehydration associated with diarrhea but does not address the underlying bacterial infection.
+Oral vancomycin (Choice D) is used for treating Clostridioides difficile infections and is not effective against Salmonella typhi.
+Penicillin (Choice E) is not the first-line treatment for typhoid fever and is less effective against Salmonella typhi compared to ciprofloxacin.
+
+Given the patient’s travel history, clinical presentation, and the need for effective antibiotic therapy, Ciprofloxacin (Choice A) is the most appropriate treatment for this patient's condition. 
 
 Answer: A
 
-Question: A 43-year-old man with a history of untreated HIV presents with fever, shortness of breath, and a nonproductive cough for the past week. Past medical history is significant for HIV diagnosed 10 years ago and never treated. His most recent CD4+ T cell count was 105/µL. Physical examination reveals bilateral crepitus over all lobes. No lymphadenopathy is present. A chest radiograph reveals bilateral infiltrates. Which of the following is the best treatment for this patient?
+[Question] 
+A 43-year-old man with a history of untreated HIV presents with fever, shortness of breath, and a nonproductive cough for the past week. Past medical history is significant for HIV diagnosed 10 years ago and never treated. His most recent CD4+ T cell count was 105/µL. Physical examination reveals bilateral crepitus over all lobes. No lymphadenopathy is present. A chest radiograph reveals bilateral infiltrates. Which of the following is the best treatment for this patient?
 
-Choices: A: Highly active antiretroviral therapy (HAART), B: Trimethoprim-sulfamethoxazole, C: Ganciclovir, D: Azithromycin, E: Amphotericin B
+[Choices] 
+A: Highly active antiretroviral therapy (HAART), B: Trimethoprim-sulfamethoxazole, C: Ganciclovir, D: Azithromycin, E: Amphotericin B
 
-Answer: B
+[Summary] 
+A 21-year-old man presents with fever, diarrhea, abdominal tenderness, and a pink macular rash after constipation, requiring monitoring of vital signs after a summer trip.
 
-Question: A 57-year-old man presents to his primary care physician with a 2-month history of right upper and lower extremity weakness. He noticed the weakness when he started falling far more frequently while running errands. Since then, he has had increasing difficulty with walking and lifting objects. His past medical history is significant only for well-controlled hypertension, but he says that some members of his family have had musculoskeletal problems. His right upper extremity shows forearm atrophy and depressed reflexes while his right lower extremity is hypertonic with a positive Babinski sign. Which of the following is most likely associated with the cause of this patient's symptoms?
+[Explanation] 
+This 43-year-old man with untreated HIV presents with fever, shortness of breath, and a nonproductive cough, symptoms that have been ongoing for the past week. His CD4+ T cell count is notably low at 105/µL, indicating severe immunosuppression. Physical examination reveals bilateral crepitus over all lung lobes, and a chest radiograph shows bilateral infiltrates.
+In the context of HIV/AIDS and a CD4+ T cell count below 200/µL, these symptoms are highly suggestive of Pneumocystis jirovecii pneumonia (PJP), a common opportunistic infection in immunocompromised individuals, particularly those with untreated HIV/AIDS.
+The standard treatment for PJP is Trimethoprim-sulfamethoxazole (Bactrim). This antibiotic is effective against Pneumocystis jirovecii and is the first-line treatment for this condition.
+Other Choices:
+Highly active antiretroviral therapy (HAART) (Choice A) is crucial for long-term management of HIV/AIDS but is not the immediate treatment for the acute infection described here.
+Ganciclovir (Choice C) is an antiviral medication used to treat cytomegalovirus (CMV) infections, not typically for PJP.
+Azithromycin (Choice D) is an antibiotic that might be used for bacterial infections or prophylaxis for Mycobacterium avium complex (MAC) but is not the treatment of choice for PJP.
+Amphotericin B (Choice E) is an antifungal medication used for severe fungal infections such as cryptococcal meningitis, not typically for PJP.
 
-Choices: A: HLA-B8 haplotype, B: HLA-DR2 haplotype, C: Mutation in SOD1, D: Mutation in SMN1, E: Viral infection
+Thus, given the patient's symptoms, history, and radiographic findings, the best treatment option is Trimethoprim-sulfamethoxazole (B).
 
-Answer:"""
+[Answer] 
+B
+
+[Question] 
+A 57-year-old man presents to his primary care physician with a 2-month history of right upper and lower extremity weakness. He noticed the weakness when he started falling far more frequently while running errands. Since then, he has had increasing difficulty with walking and lifting objects. His past medical history is significant only for well-controlled hypertension, but he says that some members of his family have had musculoskeletal problems. His right upper extremity shows forearm atrophy and depressed reflexes while his right lower extremity is hypertonic with a positive Babinski sign. Which of the following is most likely associated with the cause of this patient's symptoms?
+
+[Choices] 
+A: HLA-B8 haplotype, B: HLA-DR2 haplotype, C: Mutation in SOD1, D: Mutation in SMN1, E: Viral infection
+
+[Summary]
+"""
 
 correct_answer = "C"
 choices = ["A", "B", "C", "D", "E"]
