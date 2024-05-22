28a8086e5cc90571d0614bc7d95ade899ddf7c9a
jayson1200 <jaysonmeribe2004@gmail.com>
2024-05-21 18:05:42 -0700

    stuff

---
+++ evaluators/mcq_evaluator.py
@@ -0,0 +1,13 @@
+class MCQEvaluator:
+    def __init__(self):
+        self.num_questions_per_batch = []
+        self.num_correct_per_batch = []
+
+    
+    def print_curr_acc(self):
+        pass
+
+    def print_add_acc(self, correct_choices, responses):
+        pass
+        
+    
